70979a87daa2b860743823941e384b6b6aad6d6f
Dexorp <jordanforrester123@gmail.com>
2022-10-20 19:11:43 -0400

    minorupdate

--- client/src/components/Post.vue
+++ client/src/components/Post.vue
@@ -9,6 +9,7 @@
         <div class="media-left">
             <figure class="image is-64x64">
                 <img src="https://bulma.io/images/placeholders/128x128.png" alt="Image">
+                
             </figure>
         </div>
 
